d3bb538c9695bf66346b353707d83d40154f8203
EsterIso <esterlinjpaulino@gmail.com>
2025-09-20 23:56:19 -0400

    project completed

--- server/public/scripts/villagers.js
+++ server/public/scripts/villagers.js
@@ -11,7 +11,13 @@ const renderVillagers = async() => {
             data.map(villager => {
                 const villagerCard = document.createElement('div');
                 villagerCard.className = 'villager-card';
-                villagerCard.textContent = '------ Passport ------'
+                villagerCard.onclick = () => {
+                    window.location.href = `/villagers/${villager.name}`;
+                };
+                
+                const cardHeader = document.createElement('p');
+                cardHeader.className = 'card-header';
+                cardHeader.textContent = '------ Passport ------';
 
                 const content = document.createElement('div')
                 content.className = "card-content";
@@ -40,18 +46,69 @@ const renderVillagers = async() => {
                 villagerBday.className = 'villager-bday';
                 villagerBday.textContent = villager.birthday;
                 rightContainer.appendChild(villagerBday);
-
+                
                 content.appendChild(leftContainer);
                 content.appendChild(rightContainer);
 
+                villagerCard.appendChild(cardHeader);
                 villagerCard.appendChild(content);
 
                 mainContent.appendChild(villagerCard);
             })
         } else {
-
+            const h2 = document.createElement('h2');
+            h2.textContent = 'No Villager Available 😞';
+            giftContent.appendChild(h2);
         }
 
 }
 
-renderVillagers();
+const renderVillager = async() => {
+    const requestedName = window.location.href.split('/').pop();
+    console.log('request name: ' + requestedName);
+    const res = await fetch('/villagers');
+
+    if (!res.ok) {
+        throw new Error(`Requested failed: ${res.status} ${res.statusText}`);
+    }
+
+    const data = await res.json();
+
+    const cardContent = document.getElementsByClassName('card-content')[0];
+
+    let villager;
+
+    if (data) {
+        villager = data.find(villager => villager.name === requestedName);
+        if (!villager) {
+            window.location.href = '/404.html';
+            return;
+        }
+    }
+    console.log(villager);
+    if(villager) {
+        document.getElementById('image').src = villager.photo;
+        document.getElementById('villager-name').textContent = villager.name;
+        document.getElementById('villager-gender').textContent = villager.gender;
+        document.getElementById('villager-species').textContent = villager.species;
+        document.getElementById('villager-personality').textContent = villager.personality;
+        document.getElementById('villager-catchphrase').textContent = villager.catchphrase;
+        document.getElementById('villager-hobby').textContent = villager.hobby;
+        document.getElementById('villager-bday').textContent = villager.birthday;    
+        document.title = `Villager - ${villager.name}`;    
+    } else {
+        const h2 = document.createElement('h2');
+        h2.textContent = 'No Villager Available 😞';
+        // cardContent.appendChild(h2);
+    }
+}
+
+const requestedUrl = window.location.pathname.split('/').filter(Boolean).pop();
+
+if (!requestedUrl || requestedUrl === 'villagers') {
+    renderVillagers();
+} else {
+    renderVillager();
+}
+
+
